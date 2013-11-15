777931c2af9405307150f00617718ce1b120321b
Denis Chancogne <denis.chancogne@free.fr>
2013-11-15 16:54:30 +0100

    Cleaning.

--- src/file_list.sh
+++
@@ -1,4 +0,0 @@
-
-SOAF_SRC_FILE_LIST="util.sh log.sh cfg.sh init.sh version.sh"
-SOAF_SRC_FILE_LIST="$SOAF_SRC_FILE_LIST display.sh roll.sh"
-SOAF_SRC_FILE_LIST="$SOAF_SRC_FILE_LIST engine.sh dist.sh"
