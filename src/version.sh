e6f21fbeba22cbeceb37e07dfc217d7156df8a20
Denis Chancogne <denis.chancogne@free.fr>
2014-04-27 22:01:47 +0200

    Fix beta version number.

--- src/version.sh
+++ src/version.sh
@@ -10,7 +10,7 @@ soaf_info_add_var() {
 ################################################################################
 
 SOAF_NAME="soaf.sh"
-SOAF_VERSION="0.2.0"
+SOAF_VERSION="0.2.0_b_1"
 
 soaf_info_add_var SOAF_VERSION
 
