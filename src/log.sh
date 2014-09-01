b1c0f2936457ec2e37d14c67c724f0e5a4c7ed90
Denis Chancogne <denis.chancogne@free.fr>
2014-09-01 19:05:20 +0200

    Log feature is now fully configurable.

--- src/log.sh
+++ src/log.sh
@@ -6,9 +6,12 @@ SOAF_LOG_WARN="WARN_"
 SOAF_LOG_INFO="INFO_"
 SOAF_LOG_DEBUG="DEBUG"
 
+SOAF_LOG_NATURE_INT="soaf.log"
 SOAF_LOG_ROLL_NATURE_INT="soaf.log.roll"
 
 SOAF_LOG_LEVEL_ATTR="soaf_log_level"
+
+SOAF_LOG_FN_ATTR="soaf_log_fn"
 
 ################################################################################
 ################################################################################
@@ -18,6 +21,7 @@ soaf_log_cfg() {
 	###---------------
 	soaf_cfg_set SOAF_LOG_LEVEL $SOAF_LOG_INFO
 	###---------------
+	soaf_cfg_set SOAF_LOG_NATURE_LIST $SOAF_LOG_NATURE_INT
 	soaf_cfg_set SOAF_LOG_ROLL_NATURE $SOAF_LOG_ROLL_NATURE_INT
 	###---------------
 	local USER_NAME=$(soaf_map_get $USER_NATURE $SOAF_USER_NAME_ATTR)
@@ -27,18 +31,33 @@ soaf_log_cfg() {
 soaf_log_init() {
 	soaf_info_add_var "SOAF_LOG_LEVEL SOAF_LOG_FILE"
 	###---------------
-	[ $SOAF_LOG_ROLL_NATURE = $SOAF_LOG_ROLL_NATURE_INT ] && \
-		soaf_create_roll_cond_gt_nature $SOAF_LOG_ROLL_NATURE $SOAF_LOG_FILE
+	soaf_create_log_nature $SOAF_LOG_NATURE_INT soaf_log_int
+	soaf_create_roll_cond_gt_nature $SOAF_LOG_ROLL_NATURE_INT $SOAF_LOG_FILE
 }
 
 soaf_log_prepenv() {
-	[ $SOAF_LOG_ROLL_NATURE = $SOAF_LOG_ROLL_NATURE_INT ] && \
-		mkdir -p $(dirname $SOAF_LOG_FILE)
+	local nature
+	for nature in $SOAF_LOG_NATURE_LIST
+	do
+		if [ "$nature" = "$SOAF_LOG_NATURE_INT" ]
+		then
+			mkdir -p $(dirname $SOAF_LOG_FILE)
+		fi
+	done
 }
 
 soaf_engine_add_cfg_fn soaf_log_cfg
 soaf_engine_add_init_fn soaf_log_init
 soaf_engine_add_prepenv_fn soaf_log_prepenv
+
+################################################################################
+################################################################################
+
+soaf_create_log_nature() {
+	local NATURE=$1
+	local FN=$2
+	soaf_map_extend $NATURE $SOAF_LOG_FN_ATTR $FN
+}
 
 ################################################################################
 ################################################################################
@@ -73,7 +92,7 @@ soaf_log_num_level() {
 ################################################################################
 ################################################################################
 
-soaf_log() {
+soaf_log_int() {
 	local LEVEL=$1
 	local MSG=$2
 	local NAME=$3
@@ -95,6 +114,21 @@ soaf_log() {
 [$(date '+%x_%X')][$LEVEL]  $MSG
 _EOF_
 	fi
+}
+
+################################################################################
+################################################################################
+
+soaf_log() {
+	local LEVEL=$1
+	local MSG=$2
+	local NAME=$3
+	local nature
+	for nature in $SOAF_LOG_NATURE_LIST
+	do
+		local FN=$(soaf_map_get $nature $SOAF_LOG_FN_ATTR)
+		[ -n "$FN" ] && $FN $LEVEL "$MSG" $NAME
+	done
 }
 
 ################################################################################
