4c01c84a21410c13a7099bc3a765ab6270c05f13
Denis Chancogne <denis.chancogne@free.fr>
2014-09-26 12:08:49 +0200

    Optim : only one call of soaf_log_prep_cmd_out_err() in roll feature.

--- src/roll.sh
+++ src/roll.sh
@@ -71,7 +71,6 @@ soaf_roll_no_compress() {
 soaf_roll_proc_file() {
 	local NATURE=$1
 	local FILE=$2
-	soaf_log_prep_cmd_out_err $SOAF_ROLL_LOG_NAME
 	local FILE_SIZE=$(stat -c %s $FILE 2> $SOAF_LOG_CMD_ERR_FILE)
 	soaf_log_cmd_err $SOAF_ROLL_LOG_NAME
 	[ -z "$FILE_SIZE" ] && FILE_SIZE=1
@@ -98,7 +97,6 @@ soaf_roll_clean() {
 	local SIZE=$2
 	local FILE_DN=$(dirname $FILE)
 	local FILE_BN=$(basename $FILE)
-	soaf_log_prep_cmd_out_err $SOAF_ROLL_LOG_NAME
 	local FILE_LIST=$(find $FILE_DN -name "$FILE_BN-*" -a -type f \
 		2> $SOAF_LOG_CMD_ERR_FILE | sort | head -n-$SIZE | tr '\n' ' ')
 	soaf_log_cmd_err $SOAF_ROLL_LOG_NAME
@@ -113,6 +111,7 @@ soaf_roll_nature() {
 	local FILE=$(soaf_map_get $NATURE $SOAF_ROLL_FILE_ATTR)
 	if [ -n "$FILE" ]
 	then
+		soaf_log_prep_cmd_out_err $SOAF_ROLL_LOG_NAME
 		local SIZE=$(soaf_map_get $NATURE $SOAF_ROLL_SIZE_ATTR $SOAF_ROLL_SIZE)
 		if [ -f "$FILE" ]
 		then
