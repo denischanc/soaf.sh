95b23d14b1f6400f6eb440dff5620104ab4560fc
Denis Chancogne <denis.chancogne@free.fr>
2019-02-19 17:17:17 +0100

    Use _RET variables io subshell.

--- src/util_list.sh
+++ src/util_list.sh
@@ -5,9 +5,9 @@ soaf_pmp_list_var() {
 	local POS=$1
 	local VAR_PRE=$2
 	case $POS in
-		$SOAF_POS_PRE) echo "__${VAR_PRE}__soaf_pmp_pre_list";;
-		$SOAF_POS_POST) echo "__${VAR_PRE}__soaf_pmp_post_list";;
-		*) echo "__${VAR_PRE}__soaf_pmp_main_list";;
+		$SOAF_POS_PRE) SOAF_RET_LIST=__${VAR_PRE}__soaf_pmp_pre_list;;
+		$SOAF_POS_POST) SOAF_RET_LIST=__${VAR_PRE}__soaf_pmp_post_list;;
+		*) SOAF_RET_LIST=__${VAR_PRE}__soaf_pmp_main_list;;
 	esac
 }
 
@@ -15,19 +15,19 @@ soaf_pmp_list_fill() {
 	local POS=$1
 	local VAR_PRE=$2
 	local ELMT_LIST=$3
-	local VAR=$(soaf_pmp_list_var "$POS" $VAR_PRE)
-	eval $VAR=\"\$$VAR \$ELMT_LIST\"
+	soaf_pmp_list_var "$POS" $VAR_PRE
+	eval $SOAF_RET_LIST=\"\$$SOAF_RET_LIST \$ELMT_LIST\"
 }
 
 soaf_pmp_list_cat() {
 	local VAR_PRE=$1
-	SOAF_RET_LIST=""
-	local pos VAR
+	local RET_LIST pos
 	for pos in $SOAF_POS_PRE $SOAF_POS_MAIN $SOAF_POS_POST
 	do
-		VAR=$(soaf_pmp_list_var $pos $VAR_PRE)
-		eval SOAF_RET_LIST=\"\$SOAF_RET_LIST\$$VAR\"
+		soaf_pmp_list_var $pos $VAR_PRE
+		eval RET_LIST=\"\$RET_LIST\$$SOAF_RET_LIST\"
 	done
+	SOAF_RET_LIST=$RET_LIST
 }
 
 ################################################################################
