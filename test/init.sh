b640aedd832eddc8b627170b9ba10f27c374e529
Denis Chancogne <denis.chancogne@free.fr>
2020-05-12 20:46:15 +0200

    Update mail notif test.

--- test/init.sh
+++ test/init.sh
@@ -8,3 +8,12 @@ then
 	soaf_create_notif_sms_free_nature "test.notif.sms.free" \
 		"test.sms.free.account"
 fi
+
+### Copy/paste next line in init-mail.sh and replace [mail_to_addr] :
+### MAIL_TO_ADDR="[mail_to_addr]"
+TEST_INIT_MAIL_FILE=$TEST_HOME/init-mail.sh
+if [ -f $TEST_INIT_MAIL_FILE ]
+then
+	. $TEST_INIT_MAIL_FILE
+	soaf_create_notif_mail_nature "test.notif.mail" "$MAIL_TO_ADDR"
+fi
